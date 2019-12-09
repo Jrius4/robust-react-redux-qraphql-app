31b9d3327e430516c14b38c38c95af6db515b237
Jrius Kaxibwe <kazibwejuliusjunior@gmail.com>
2019-12-09 08:38:07 +0300

    Updates

---
+++ precache-manifest.fbdd3738948027fb8a3554f3645122ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992665fea535791015c5283efe58b830",
+    "url": "/robust-react-redux-qraphql-app/index.html"
+  },
+  {
+    "revision": "165a993c0ec3c3d1c8d5",
+    "url": "/robust-react-redux-qraphql-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/robust-react-redux-qraphql-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "165a993c0ec3c3d1c8d5",
+    "url": "/robust-react-redux-qraphql-app/static/js/main.15b7ade0.chunk.js"
+  },
+  {
+    "revision": "9a758d5a0d09d2593c4c",
+    "url": "/robust-react-redux-qraphql-app/static/js/runtime~main.7bc5b65b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robust-react-redux-qraphql-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
