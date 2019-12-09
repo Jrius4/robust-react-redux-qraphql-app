5d3cd0f751d3e6af768d8701af7e4ad99ea59252
Jrius Kaxibwe <kazibwejuliusjunior@gmail.com>
2019-12-09 09:10:01 +0300

    build github pages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Build with React
         </a>
       </header>
     </div>
